42e57c59b587959872da9bb6e7bc1c8b7bc712f2
theory friction practice <jackson.brebnor@gmail.com>
2017-01-16 21:50:46 -0500

    completes socket

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('chat message', (msg) => {
     console.log(`message: ${msg}`);
+    io.emit('chat message', msg);
   });
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
